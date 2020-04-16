4da5c33ffc9460c8fbfcdd748d7a9db8421a1e24
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-16 12:38:59 +0700

    add validation message to unique errors

--- server/middlewares/error_handler.js
+++ server/middlewares/error_handler.js
@@ -1,5 +1,5 @@
 function errorHandler(err, req, res, next) {
-  if (err.name === 'SequelizeValidationError') {
+  if (err.name === 'SequelizeValidationError' || err.name == 'SequelizeUniqueConstraintError') {
     res.status(400).json({ message: err.errors[0].message });
   } else if (err.message) {
     res.status(404).json({ message: err.message });
