fe2e255f2c3c4602b9f6981bc339a43fc38db6c3
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-24 02:05:34 +0700

    Summary of this commit: - Normal users ( without admin privileges ) shouldn't login - User register tests is replaced by admin test case

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,9 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-const { decryptPassword } = require('../helpers/bcrypt');
 
 class UserController {
   static async login(req, res, next) {
     const loginData = req.body;
-    let userData = null;
 
     User.findOne({
       where: {
@@ -29,24 +27,7 @@ class UserController {
             res.status(200).json({ message: 'Welcome admin!', token, isAdmin: true });
           }
         } else {
-          userData = user;
-
-          const decrypt = decryptPassword(loginData.password, user.password);
-          return decrypt;
-        }
-      })
-      .then((decrypted) => {
-        if (decrypted) {
-          const token = jwt.sign(
-            {
-              UserId: userData.id,
-              Role: userData.role,
-            },
-            process.env.JWT_SECRET
-          );
-          res.status(200).json({ message: `Welcome ${userData.email}`, token });
-        } else {
-          throw new Error('Wrong password');
+          throw new Error('User is not an Admin!');
         }
       })
       .catch(next);
--- server/tests/user.js
+++ server/tests/user.js
@@ -1,22 +1,8 @@
 const request = require('supertest');
 const app = require('../app_test');
-const { User } = require('../models');
-
-const correctForm = {
-  email: 'padoel@cenah.com',
-  username: 'padoel',
-  password: 'jancok123',
-  role: 'user',
-};
 
 function login() {
-  beforeAll((done) => {
-    User.create(correctForm)
-      .then(() => done())
-      .catch((e) => done(e));
-  });
-
-  describe('user and admin login', () => {
+  describe('admin login', () => {
     // admin login res
     describe('admin login', () => {
       it('should send an token object with status code 200, representing admin message', (done) => {
@@ -33,24 +19,6 @@ function login() {
             expect(body).toHaveProperty('message');
             expect(body).toHaveProperty('isAdmin');
             global.token = body.token;
-            done();
-          })
-          .catch(done);
-      });
-    });
-
-    // user login res
-    describe('user login', () => {
-      it('should send an token object with status code 200, representing success message', (done) => {
-        request(app)
-          .post('/login')
-          .send(correctForm)
-          .then((res) => {
-            const { body, status } = res;
-            expect(status).toBe(200);
-            expect(body).toHaveProperty('token');
-            expect(body).toHaveProperty('message');
-            expect(body).not.toHaveProperty('isAdmin');
             done();
           })
           .catch(done);
@@ -84,7 +52,7 @@ function login() {
         request(app)
           .post('/login')
           .send({
-            username: 'padoel',
+            username: 'admin',
             password: 'asdasdasd',
           })
           .then((res) => {
@@ -97,42 +65,22 @@ function login() {
           .catch(done);
       });
     });
-  });
 
-  describe('register new user', () => {
-    // add new user
-    describe('add new user', () => {
-      it('should add new user in database and send status of 201 also a success message', (done) => {
+    // not an admin
+    describe('normal login', () => {
+      it('should throw an error because user is not an admin', (done) => {
         request(app)
-          .post('/register')
-          .send(correctForm)
-          .then((res) => {
-            const { body, status } = res;
-            expect(status).toBe(201);
-            expect(body).toHaveProperty('success');
-            expect(body).toHaveProperty('message');
-            expect(body).not.toHaveProperty('token');
-            done();
-          })
-          .catch(done);
-      });
-    });
-
-    // bad request on register
-    describe('add new user but bad form request', () => {
-      it('should not add new user and send error', (done) => {
-        request(app)
-          .post('/register')
+          .post('/login')
           .send({
-            email: '',
-            password: '1',
+            username: 'padoel',
+            password: 'asdasdasd',
           })
           .then((res) => {
             const { body, status } = res;
-            expect(status).toBe(400);
-            expect(body).toHaveProperty('message');
+            expect(status).toBe(404);
             expect(body).not.toHaveProperty('token');
-            expect(body).not.toHaveProperty('success');
+            expect(body).not.toHaveProperty('isAdmin');
+            expect(body).toHaveProperty('message');
             done();
           })
           .catch(done);
